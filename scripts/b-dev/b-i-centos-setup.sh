089550a343546606616040946232d15816628806
Tom Wilford <hello@jollyblueman.com>
2021-01-31 15:08:32 +0000

    Added memcached

--- scripts/b-dev/b-i-centos-setup.sh
+++ scripts/b-dev/b-i-centos-setup.sh
@@ -44,6 +44,7 @@ php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
 HASH="$(wget -q -O - https://composer.github.io/installer.sig)"
 php -r "if (hash_file('SHA384', 'composer-setup.php') === '$HASH') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
 sudo php composer-setup.php --install-dir=/usr/local/bin --filename=composer
+composer init
 
 # memcached
 yum -y install memcached
