a47ebb34e07c4f116bc837d1eb22a4b422028299
Tom Wilford <hello@jollyblueman.com>
2023-03-23 20:56:56 +0000

    reorganisation and adding rocky

---
+++ scripts/virtual-machines/centos7/centos7-vagrant-file.sh
@@ -0,0 +1,19 @@
+Vagrant.configure("2") do |config|
+
+  config.vm.box = "centos/7"
+
+    config.vm.network "forwarded_port", guest: 80, host: 8081, auto_correct: true
+    config.vm.network "forwarded_port", guest: 3306, host: 3307
+
+    config.vm.network "private_network", ip: "192.168.33.10"
+
+    config.vm.network "public_network"
+
+    config.vm.synced_folder ".", "/var/www/html"
+
+    config.vm.provider "virtualbox" do |vb|
+      vb.gui = false
+      vb.memory = "1024"
+      vb.customize ["modifyvm", :id, "--natdnshostresolver1", "on"]
+    end
+end
