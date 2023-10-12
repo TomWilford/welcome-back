5d71fab82aa069e9ff681c96ad4854a38fda4267
Tom Wilford <hello@jollyblueman.com>
2023-10-12 16:38:09 +0100

    Update linux-local-development.sh
    Updating php

--- scripts/local-os/linux-local-development.sh
+++ scripts/local-os/linux-local-development.sh
@@ -7,8 +7,8 @@ sudo apt-get update
 sudo apt-get -y install apache2
 
 # PHP
-sudo apt-get -y install php7.4 libapache2-mod-php7.4
-sudo apt-get -y install curl zip unzip php7.4-mysql php7.4-curl php7.4-ctype php7.4-uuid php7.4-iconv php7.4-json php7.4-mbstring php7.4-gd php7.4-intl php7.4-xml php7.4-zip php7.4-gettext php7.4-pgsql php7.4-bcmath php7.4-redis php-memcached php7.4-zip php7.4-xml php-simplexml php-mbstring
+sudo apt-get -y install php8.2 libapache2-mod-php8.2
+sudo apt-get -y install curl zip unzip php8.2-mysql php8.2-curl php8.2-ctype php8.2-uuid php8.2-iconv php8.2-json php8.2-mbstring php8.2-gd php8.2-intl php8.2-xml php8.2-zip php8.2-gettext php8.2-pgsql php8.2-bcmath php8.2-redis php8.2-memcached php8.2-zip php8.2-xml php8.2-simplexml php8.2-mbstring
 sudo phpenmod curl
 
 # PEAR
